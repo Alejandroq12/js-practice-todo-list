2bf9c5d3cc9c00dbaba8d438f9dbd5d715ca65ad
alejandroq12 <jaquezada@innovacion.gob.sv>
2024-07-25 11:15:29 -0600

    Feat: Add display todo dy namically.

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,14 +1,21 @@
 import html from './app.html?raw';
+import todoStore from '../store/todo.store.js';
 
 /**
  * 
  * @param {String} elementId 
  */
 export const App = (elementId) => {
+
+  const displayTodos = () => {
+    const todos = todoStore.getTodos(todoStore.getCurrentFilter());
+    console.log(todos);
+  }
   // When the App() function it is called
   (() => {
     const app = document.createElement('div');
     app.innerHTML = html;
     document.querySelector(elementId).append(app);
+    displayTodos();
   })();
 }
